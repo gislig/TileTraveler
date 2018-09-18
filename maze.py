4a41d3af2cbbfcdc13a29f6672bfd0b0dbb9e818
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-18 17:40:21 +0000

    Beinagrindin komin aftur

--- maze.py
+++ maze.py
@@ -2,17 +2,34 @@
 #You can travel: (N)orth or (E)ast or (S)outh or (W)est..
 #Victory!
 
-location = 1,1
+new_location_x = 1
+new_location_y = 1
 
 print("You can travel: (N)orth.")
 while True:
-    
+
     direction = input("Direction: ")
     if direction in "wW":
-        print("Going West")
-    if direction in "nN":
-        print("Going North")
-    if direction in "sS":
-        print("Going South")
+        if new_location_x > 1:
+            new_location_x -= 1
+            print(str(new_location_x))
+        else:
+            print("Not a valid direction!")
     if direction in "eE":
-        print("Going East")
+        if new_location_x < 3:
+            new_location_x += 1
+            print(str(new_location_x))
+        else:
+            print("Not a valid direction!")
+    if direction in "nN":
+        if new_location_y > 1:
+            new_location_y -= 1
+            print(str(new_location_y))
+        else:
+            print("Not a valid direction!")
+    if direction in "sS":
+        if new_location_y < 3:
+            new_location_y += 1
+            print(str(new_location_y))
+        else:
+            print("Not a valid direction!")
