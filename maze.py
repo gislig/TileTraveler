5a293a2b016be97b42b76de2dc349c97738d39a4
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-18 17:23:49 +0000

    Beinagrindin komin

--- maze.py
+++ maze.py
@@ -1,5 +1,18 @@
+
+#You can travel: (N)orth or (E)ast or (S)outh or (W)est..
+#Victory!
+
 location = 1,1
 
-while new_location <= 3,3:
-    print("HELLO World")
-    new_location = +1,+1
+print("You can travel: (N)orth.")
+while True:
+    
+    direction = input("Direction: ")
+    if direction in "wW":
+        print("Going West")
+    if direction in "nN":
+        print("Going North")
+    if direction in "sS":
+        print("Going South")
+    if direction in "eE":
+        print("Going East")
