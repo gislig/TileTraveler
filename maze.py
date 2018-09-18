56bbbb5fd8d13218e639da7d75133c39893d3d49
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-18 18:39:09 +0000

    Final kóði, svona næstum því

--- maze.py
+++ maze.py
@@ -1,58 +1,101 @@
-
 #You can travel: (N)orth or (E)ast or (S)outh or (W)est..
 #Victory!
 
 new_location_x = 1
 new_location_y = 1
 
-old_location_x = 1
-old_location_y = 1
+allowTravel_North = True
+allowTravel_South = False
+allowTravel_East = False
+allowTravel_West = False
 
 print("You can travel: (N)orth.")
 while True:
 
-    old_location_x = new_location_x
-    old_location_y = new_location_y
-
-    print("Current location : {},{}".format(new_location_x,new_location_y))
+    #print("Previous location : {},{}".format(new_location_x,new_location_y))
     direction = input("Direction: ")
-    if direction in "wW":
+
+    if direction in "wW" and allowTravel_West == True:
         if new_location_x > 1:
             new_location_x -= 1
         else:
             print("Not a valid direction!")
-    if direction in "eE":
+    elif direction in "wW" and allowTravel_West == False:
+        print("Not a valid direction!")
+
+    if direction in "eE" and allowTravel_East == True:
         if new_location_x < 3:
             new_location_x += 1
         else:
             print("Not a valid direction!")
-    if direction in "nN":
+    elif direction in "eE" and allowTravel_East == False:
+        print("Not a valid direction!")
+
+    if direction in "nN" and allowTravel_North == True:
         if new_location_y >= 1 and new_location_y < 3:
             new_location_y += 1
         else:
             print("Not a valid direction!")
-    if direction in "sS":
+    elif direction in "nN" and allowTravel_North == False:
+        print("Not a valid direction!")
+
+    if direction in "sS" and allowTravel_South == True:
         if new_location_y > 1 and new_location_y <= 3:
             new_location_y -= 1
         else:
             print("Not a valid direction!")
+    elif direction in "sS" and allowTravel_South == False:
+        print("Not a valid direction!")
 
+    ##################################################3
     if(new_location_x == 1 and new_location_y == 1):
         print("You can travel: (N)orth.")
+        allowTravel_North = True
+        allowTravel_South = False
+        allowTravel_East = False
+        allowTravel_West = False
     if(new_location_x == 1 and new_location_y == 2):
         print("You can travel: (N)orth (S)outh (E)ast.")
+        allowTravel_North = True
+        allowTravel_South = True
+        allowTravel_East = True
+        allowTravel_West = False
     if(new_location_x == 1 and new_location_y == 3):
         print("You can travel: (S)outh (E)ast.")
+        allowTravel_North = False
+        allowTravel_South = True
+        allowTravel_East = True
+        allowTravel_West = False
     if(new_location_x == 2 and new_location_y == 1):
         print("You can travel: (N)orth.")
+        allowTravel_North = True
+        allowTravel_South = False
+        allowTravel_East = False
+        allowTravel_West = False
     if(new_location_x == 2 and new_location_y == 2):
         print("You can travel: (W)est (S)outh.")
+        allowTravel_North = False
+        allowTravel_South = True
+        allowTravel_East = False
+        allowTravel_West = True
     if(new_location_x == 2 and new_location_y == 3):
         print("You can travel: (W)est (E)ast.")
+        allowTravel_North = False
+        allowTravel_South = False
+        allowTravel_East = True
+        allowTravel_West = True
     if(new_location_x == 3 and new_location_y == 1):
         print("Victory!")
         break
     if(new_location_x == 3 and new_location_y == 2):
         print("You can travel: (N)orth (S)outh.")
+        allowTravel_North = True
+        allowTravel_South = True
+        allowTravel_East = False
+        allowTravel_West = False
     if(new_location_x == 3 and new_location_y == 3):
         print("You can travel: (W)est (S)outh.")
+        allowTravel_North = False
+        allowTravel_South = True
+        allowTravel_East = True
+        allowTravel_West = False
