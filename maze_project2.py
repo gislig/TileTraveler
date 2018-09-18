57f13f94ef979724e33a127ba05ae8f2323aaf87
Gísli Guðmundsson <gisli.gudmundsson@advania.is>
2018-09-18 20:12:15 +0000

    Added finished project 2

--- maze_project2.py
+++ maze_project2.py
@@ -1,6 +1,18 @@
+def SetSteps(current_step, add_or_remove_step):
+    valid_step = True
+    if add_or_remove_step in "nNeE" and current_step+1 > 1 and current_step+1 <= 3:
+        current_step += 1
+    elif add_or_remove_step in "sSwW" and current_step-1 >= 1 and current_step-1 <= 3:
+        current_step -= 1
+    else:
+        print("Not a valid direction!")
+        valid_step = False
+
+    return current_step, valid_step
 
 new_location_x = 1
 new_location_y = 1
+valid_direction = False
 
 allowTravel_North = True
 allowTravel_South = False
@@ -9,51 +21,37 @@ allowTravel_West = False
 
 print("You can travel: (N)orth.")
 while True:
-
-    #print("Previous location : {},{}".format(new_location_x,new_location_y))
     direction = input("Direction: ")
-
-    if direction in "wW" and allowTravel_West == True:
-        if new_location_x > 1 and new_location_x <= 3:
-            new_location_x -= 1
-        else:
-            print("Not a valid direction!")
-            continue
-    elif direction in "wW" and allowTravel_West == False:
+    if direction in 'nN' and allowTravel_North == True:
+        new_location_y, valid_direction = SetSteps(new_location_y, direction)
+    elif direction in 'nN' and allowTravel_North == False:
         print("Not a valid direction!")
         continue
 
-    if direction in "eE" and allowTravel_East == True:
-        if new_location_x >= 1 and new_location_x < 3:
-            new_location_x += 1
-        else:
-            print("Not a valid direction!")
-            continue
-    elif direction in "eE" and allowTravel_East == False:
+    if direction in 'eE' and allowTravel_East == True:
+        new_location_x, valid_direction = SetSteps(new_location_x, direction)
+    elif direction in 'eE' and allowTravel_East == False:
         print("Not a valid direction!")
         continue
 
-    if direction in "nN" and allowTravel_North == True:
-        if new_location_y >= 1 and new_location_y < 3:
-            new_location_y += 1
-        else:
-            print("Not a valid direction!")
-            continue
-    elif direction in "nN" and allowTravel_North == False:
+    if direction in 'sS' and allowTravel_South == True:
+        new_location_y, valid_direction = SetSteps(new_location_y, direction)
+    elif direction in 'sS' and allowTravel_South == False:
         print("Not a valid direction!")
         continue
 
-    if direction in "sS" and allowTravel_South == True:
-        if new_location_y > 1 and new_location_y <= 3:
-            new_location_y -= 1
-        else:
-            print("Not a valid direction!")
-            continue
-    elif direction in "sS" and allowTravel_South == False:
+    if direction in 'wW' and allowTravel_West == True:
+        new_location_x, valid_direction = SetSteps(new_location_x, direction)
+    elif direction in 'wW' and allowTravel_West == False:
         print("Not a valid direction!")
         continue
+    
+    #print("Previous location : {},{}".format(new_location_x,new_location_y))
 
-    ##################################################3
+    if valid_direction == False:
+        continue
+
+    ##################################################
     if(new_location_x == 1 and new_location_y == 1):
         print("You can travel: (N)orth.")
         allowTravel_North = True
